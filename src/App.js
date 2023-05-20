85b434092343066959e185983e12ef46d77ca3bc
Brian Alegre <brialegre@yahoo.com>
2023-05-19 21:22:24 -0700

    Updated Search Component, added AsyncPaginate, and UseState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+// Import components
 import './App.css';
 import Search from './components/search/search';
 
+
 function App() {
+
+  // Handle On Search Change
+  const handleOnSearchChange = (searchData) => {
+    console.log(searchData)
+  }
+
   return (
     <div className="search-container">
-      <Search />
+      {/* Search Component */}
+      <Search onSearchChange={handleOnSearchChange} />
     </div>
   )
 }
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,6 +1,28 @@
+// Import necessary libraries
+import { AsyncPaginate } from "react-select-async-paginate";
+import { useState } from "react";
+
 // Search Component
-const Search = () => {
-    return ("Hello");
+const Search = ({ onSearchChange }) => {
+
+    // Search State
+    const [search, setSearch] = useState(null)
+
+    // Handle On Change
+    const handleOnChange = (searchData) => {
+        setSearch(searchData);
+        onSearchChange(searchData);
+    }
+
+    return (
+        <AsyncPaginate
+            placeholder="Search for city..."
+            // Milliseconds to wait before sending a request
+            debounceTimeout={600}
+            value={search}
+            onChange={handleOnChange}
+        />
+    )
 }
 
 // Export Component
