79d9c1a6837c22a6b70471559cb270145eb1b13a
Brian Alegre <brialegre@yahoo.com>
2023-05-19 21:06:08 -0700

    Successfully added Search Component to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Search from './components/search/search';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-green-500">
-      Hello world!
-    </h1>
+    <div className="App">
+      <Search />
+    </div>
   )
 }
 
