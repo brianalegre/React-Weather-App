7db0435e2fb3ab441f5fea1b7ec5bf608b1e3729
Brian Alegre <brialegre@yahoo.com>
2023-05-21 21:23:47 -0700

    Creating Current-Weather component

---
+++ src/components/current-weather/current-weahter.js
@@ -0,0 +1,10 @@
+// Import
+import "./current-weather.css"
+
+// Current-Weateher Component
+const CurrentWeather = () => {
+    return "hello";
+}
+
+// Export Component
+export default CurrentWeather;
