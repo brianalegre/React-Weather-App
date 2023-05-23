ae020cd8b1fe6bb5429bc1d111436a6dce2aa201
Brian Alegre <brialegre@yahoo.com>
2023-05-22 21:20:25 -0700

    Updating Current-Weather Component with weather details.  Added icons

--- src/components/current-weather/current-weahter.js
+++ src/components/current-weather/current-weahter.js
@@ -3,7 +3,15 @@ import "./current-weather.css"
 
 // Current-Weateher Component
 const CurrentWeather = () => {
-    return "hello";
+    return (
+        <div className="weather">
+            <div className="top">
+                <p className="city">Belgrade</p>
+                <p className="weather-description">Sunny</p>
+            </div>
+            <img className="weather" src="" alt="weather" />
+        </div>
+    );
 }
 
 // Export Component
