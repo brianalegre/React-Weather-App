e89752e65253bbbe660f259364211761ccb3407b
Brian Alegre <brialegre@yahoo.com>
2023-05-23 21:25:26 -0700

    Added API calls from openWeatherAPI

--- src/App.js
+++ src/App.js
@@ -2,23 +2,39 @@
 import './App.css';
 import Search from './components/search/search';
 import CurrentWeather from './components/current-weather/current-weather';
+import { WEATHER_API_URL, WEATHER_API_KEY } from './api';
+
 
 
 function App() {
 
   // Handle On Search Change
   const handleOnSearchChange = (searchData) => {
-    console.log(searchData)
-  }
+    // console.log(searchData)
+    const [lat, lon] = searchData.value.split(" ");
 
-  return (
-    <div className="search-container">
-      {/* Search Component */}
-      <Search onSearchChange={handleOnSearchChange} />
-      {/* Current-Weather Component */}
-      <CurrentWeather />
-    </div>
-  )
+    // API Call
+    const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&${lon}&appid=${WEATHER_API_KEY}`);
+    // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+    const forecastFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&${lon}&appid=${WEATHER_API_KEY}`);
+
+    // Promise
+    Promise.all([currentWeatherFetch, forecastFetch]);
+      .then(async (response) => {
+      const weatherResponse = await response[0].json();
+      const forecastResponse = await response[1].json();
+    })
+
+}
+
+return (
+  <div className="search-container">
+    {/* Search Component */}
+    <Search onSearchChange={handleOnSearchChange} />
+    {/* Current-Weather Component */}
+    <CurrentWeather />
+  </div>
+)
 }
 
 export default App;
