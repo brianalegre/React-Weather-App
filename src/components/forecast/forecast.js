3fb8f40cb6bc01b06961ad50ee14a58484a9182f
Brian Alegre <brialegre@yahoo.com>
2023-05-26 21:20:24 -0700

    Added AccordianItemPanel items in Forecast Component

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -32,11 +32,36 @@ const Forecast = ({ data }) => {
                                 <label className="day">{forecastDays[idx]}</label>
                                 <label className="description">{item.weather[0].description}</label>
                                 <label className="min-max">{Math.round(item.main.temp_min)}°C / {Math.round(item.main.temp_max)}°C </label>
-
-
                             </AccordionItemButton>
                         </AccordionItemHeading>
-                        <AccordionItemPanel></AccordionItemPanel>
+                        <AccordionItemPanel>
+                            <div className="daily-details-grid">
+                                <div className="daily-details-grid-item">
+                                    <label>Pressure</label>
+                                    <label>{item.name.pressure}hPa</label>
+                                </div>
+                                <div className="daily-details-grid-item">
+                                    <label>Humidity</label>
+                                    <label>{item.name.humidity}%</label>
+                                </div>
+                                <div className="daily-details-grid-item">
+                                    <label>Clouds</label>
+                                    <label>{item.clouds.all}%</label>
+                                </div>
+                                <div className="daily-details-grid-item">
+                                    <label>Wind Speed:</label>
+                                    <label>{item.wind.speed} m/s </label>
+                                </div>
+                                <div className="daily-details-grid-item">
+                                    <label>Sea Level:</label>
+                                    <label>{item.main.sea_level}m</label>
+                                </div>
+                                <div className="daily-details-grid-item">
+                                    <label>Feels Like:</label>
+                                    <label>{Math.round(item.main.feels_like)}°C</label>
+                                </div>
+                            </div>
+                        </AccordionItemPanel>
 
                     </AccordionItem>
                 ))}
